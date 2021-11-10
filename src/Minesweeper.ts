da1f343d010cb456580e9fc1556e218690d8d33c
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-11-10 21:17:08 +0000

    update some stuff

--- src/Minesweeper.ts
+++ src/Minesweeper.ts
@@ -1,49 +1,57 @@
+import 'source-map-support/register.js'
+
 /**
  * Minesweeper options.
- * @typedef {object} MinesweeperOpts
- * @property {number} [rows] - The number of rows in the mine field. Defaults to 9.
- * @property {number} [columns] - The number of columns in the mine field. Defaults to 9.
- * @property {number} [mines] - The number of mines in the mine field. Defaults to 10.
- * @property {string} [emote] - The emote used as a mine. Defaults to "boom".
- * @property {boolean} [revealFirstCell] - Whether or not the first cell should be revealed (like in regular Minesweeper). Defaults to FALSE.
- * @property {boolean} [zeroFirstCell] - Whether or not the first cell revealed should always be a zero (and automatically reveal any surrounding safe cells). Does nothing if `revealFirstCell` is false. Defaults to true.
- * @property {boolean} [spaces] - Specifies whether or not the emojis should be surrounded by spaces. Defaults to true.
- * @property {'emoji' | 'code' | 'matrix'} - The type of the returned data. Defaults to "emoji".
  */
-interface MinesweeperOpts {
+export interface MinesweeperOpts {
+  /** The number of rows in the mine field. Defaults to 9. */
   rows?: number;
+
+  /** The number of columns in the mine field. Defaults to 9. */
   columns?: number;
+
+  /** The number of mines in the mine field. Defaults to 10. */
   mines?: number;
+
+  /** The emote used as a mine. Defaults to "boom". */
   emote?: string;
+
+  /** Whether or not the first cell should be revealed (like in regular Minesweeper). Defaults to false. */
   revealFirstCell?: boolean;
+
+  /** Whether or not the first cell revealed should always be a zero (and automatically reveal any surrounding safe cells). Does nothing if `revealFirstCell` is false. Defaults to true. */
   zeroFirstCell?: boolean;
+
+  /** Specifies whether or not the emojis should be surrounded by spaces. Defaults to true. */
   spaces?: boolean;
-  returnType?: 'emoji' | 'code' | 'matrix'; 
+
+  /** The type of the returned data. Defaults to "emoji". */
+  returnType?: 'emoji' | 'code' | 'matrix';
 }
 
 /**
  * Cell types.
- * @typedef {object} CellTypes
- * @property {string} mine - The definition of a mine string.
- * @property {string[]} numbers - The numbers as emote names.
  */
-interface CellTypes {
+export interface CellTypes {
+  /** The definition of a mine string. */
   mine: string;
+
+  /** The numbers as emote names. */
   numbers: string[];
 }
 
 /**
  * Safe cell. Defines the coordinates of a safe cell.
- * @typedef {object}SafeCell
- * @property {number} x - row id
- * @property {number} y - column id
  */
-interface SafeCell {
+export interface SafeCell {
+  /** row id */
   x: number;
+
+  /** column id */
   y: number;
 }
 
-class Minesweeper {
+export default class Minesweeper {
   public readonly rows: number;
   public readonly columns: number;
   public readonly mines: number;
@@ -59,9 +67,9 @@ class Minesweeper {
   /**
    * The constructor of the Minesweeper class.
    * @constructor
-   * @param {MinesweeperOpts} opts - The options of the Minesweeper class.
+   * @param opts - The options of the Minesweeper class.
    */
-  constructor(opts: MinesweeperOpts | undefined = undefined) {
+  public constructor(opts: MinesweeperOpts | undefined = undefined) {
     this.rows = (opts && opts.rows) || 9;
     this.columns = (opts && opts.columns) || 9;
     this.mines = (opts && opts.mines) || 10;
@@ -75,16 +83,15 @@ class Minesweeper {
 
     this.types = {
       mine: this.spoilerize(this.emote),
-      numbers: [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight' ].map(n => this.spoilerize(n))
+      numbers: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'].map(n => this.spoilerize(n))
     };
   }
 
   /**
    * Turns a text into a Discord spoiler.
-   * @param {string} str - The string to spoilerize.
-   * @returns {string}
+   * @param str - The string to spoilerize.
    */
-  spoilerize(str: string): string {
+  public spoilerize(str: string) {
     return this.spaces
       ? `|| :${str}: ||`
       : `||:${str}:||`;
@@ -93,8 +100,8 @@ class Minesweeper {
   /**
    * Fills the matrix with "zero" emojis.
    */
-  generateEmptyMatrix() {
-    for (let i: number = 0; i < this.rows; i++) {
+  public generateEmptyMatrix() {
+    for (let i = 0; i < this.rows; i++) {
       const arr: string[] = new Array(this.columns).fill(this.types.numbers[0]);
       this.matrix.push(arr);
     }
@@ -103,10 +110,10 @@ class Minesweeper {
   /**
    * Plants mines in the matrix randomly.
    */
-  plantMines() {
-    for (let i: number = 0; i < this.mines; i++) {
-      const x: number = Math.floor(Math.random() * this.rows);
-      const y: number = Math.floor(Math.random() * this.columns);
+  public plantMines() {
+    for (let i = 0; i < this.mines; i++) {
+      const x = Math.floor(Math.random() * this.rows);
+      const y = Math.floor(Math.random() * this.columns);
 
       if (this.matrix[x][y] === this.types.mine) {
         i--;
@@ -119,18 +126,17 @@ class Minesweeper {
   /**
    * Gets the number of mines in a particular (x, y) coordinate
    * of the matrix.
-   * @param {number} x - The x coordinate (row).
-   * @param {number} y - The y coordinate (column).
-   * @returns {string}
+   * @param x - The x coordinate (row).
+   * @param y - The y coordinate (column).
    */
-  getNumberOfMines(x: number, y: number): string {
+  public getNumberOfMines(x: number, y: number) {
     if (this.matrix[x][y] === this.types.mine) {
       return this.types.mine;
     }
 
     this.safeCells.push({ x, y });
 
-    let counter: number = 0;
+    let counter = 0;
     const hasLeft = y > 0;
     const hasRight = y < (this.columns - 1);
     const hasTop = x > 0;
@@ -165,17 +171,16 @@ class Minesweeper {
 
   /**
    * Returns the Discord message equivalent of the mine field.
-   * @returns {string}
    */
-  getTextRepresentation(): string {
-    const separator: string = this.spaces ? ' ' : '';
+  public getTextRepresentation(): string {
+    const separator = this.spaces ? ' ' : '';
     return this.matrix.map(r => r.join(separator)).join('\n');
   }
 
   /**
    * Populates the matrix.
    */
-  populate() {
+  public populate() {
     this.matrix = this.matrix.map((row, x) => {
       return row.map((col, y) => this.getNumberOfMines(x, y));
     });
@@ -183,24 +188,18 @@ class Minesweeper {
 
   /**
    * Reveal a random cell.
-   * @returns {SafeCell}
    */
-  revealFirst(): SafeCell {
-    /**
-     * Thanks to @FungiOfDeath and @TheSorton on Github for
-     * bringing this up!
-     */
-
+  public revealFirst() {
     if (!this.revealFirstCell) {
       return { x: -1, y: -1 };
     }
 
     const zeroCells = this.safeCells.filter(c => this.matrix[c.x][c.y] === this.types.numbers[0]);
     if (this.zeroFirstCell && zeroCells.length > 0) {
-      const safeCell: SafeCell = zeroCells[Math.floor(Math.random() * zeroCells.length)];
+      const safeCell = zeroCells[Math.floor(Math.random() * zeroCells.length)];
 
-      const x: number = safeCell.x;
-      const y: number = safeCell.y;
+      const x = safeCell.x;
+      const y = safeCell.y;
 
       const cell = this.matrix[x][y];
 
@@ -209,10 +208,10 @@ class Minesweeper {
 
       return { x, y };
     } else {
-      const safeCell: SafeCell = this.safeCells[Math.floor(Math.random() * this.safeCells.length)];
+      const safeCell = this.safeCells[Math.floor(Math.random() * this.safeCells.length)];
 
-      const x: number = safeCell.x;
-      const y: number = safeCell.y;
+      const x = safeCell.x;
+      const y = safeCell.y;
 
       const cell = this.matrix[x][y];
 
@@ -224,10 +223,10 @@ class Minesweeper {
 
   /**
    * Reveals all cells surrounding a cell. Only meant to be used for zero-cells during initial construction.
-   * @param {SafeCell} c - A SafeCell to reveal around. This should only be a zero-cell!
-   * @param {boolean} recurse - Whether to recursively reveal following zero-cells. Defaults to true.
+   * @param c - A SafeCell to reveal around. This should only be a zero-cell!
+   * @param recurse - Whether to recursively reveal following zero-cells. Defaults to true.
    */
-  revealSurroundings(c: SafeCell, recurse: boolean = true) {
+  public revealSurroundings(c: SafeCell, recurse = true) {
     const isSpoiler = (x: number, y: number) => this.matrix[x][y].includes("||");
     const x = c.x;
     const y = c.y;
@@ -236,7 +235,7 @@ class Minesweeper {
     const yLower = Math.max(0, y - 1);
     const xUpper = Math.min(this.rows - 1, x + 1);
     const yUpper = Math.min(this.columns - 1, y + 1);
-    let zeroCells: SafeCell[] = [];
+    const zeroCells: SafeCell[] = [];
 
     for (let i = xLower; i <= xUpper; i++) {
       for (let j = yLower; j <= yUpper; j++) {
@@ -256,9 +255,8 @@ class Minesweeper {
 
   /**
    * Generates a minesweeper mine field and returns it.
-   * @returns {(string | string[][] | null)}
    */
-  start(): string | string[][] | null {
+  public start() {
     if (this.rows * this.columns <= this.mines * 2) {
       return null;
     }
@@ -278,5 +276,3 @@ class Minesweeper {
     }
   }
 }
-
-export = Minesweeper;
